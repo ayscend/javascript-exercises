d28f153b5c978dc2a03927c269d578ad25b50fbd
Alex Y <alex.yrmlv@gmail.com>
2026-06-26 17:15:28 -0400

    complete removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    // for (const item of items) {
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (arr.includes(item)) {
+    //             arr.splice(arr.indexOf(item), 1);
+    //         }
+    //     }
+    // }
+    
+    return arr.filter(num => !(items.includes(num)));
 };
 
 // Do not edit below this line
